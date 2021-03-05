6dbd61aad78c4dd0b5d4f91e20183eb43cc7bfc6
Ty Bolchoz <tybolt@gmail.com>
2021-03-05 15:50:18 -0500

    adds basic layout for FedAdmin page

--- web/src/containers/admin/FederalAdmin.js
+++ web/src/containers/admin/FederalAdmin.js
@@ -1,53 +1,197 @@
-import React, { Fragment } from 'react';
-import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
 
-import Icon, { faUserCog, faUserPlus } from '../../components/Icons';
+import { Button, Tabs, TabPanel } from '@cmsgov/design-system';
 
-const FederalAdmin = () => {
+import {
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody
+} from '@cmsgov/design-system';
+
+import {
+  getStateAffiliations,
+  updateStateAffiliation,
+  getRoleTypes
+} from '../../actions/admin';
+
+// import { getUserStateOrTerritory } from '../../reducers/user.selector';
+
+import ManageRoleDialog from './ManageRoleDialog';
+import ConfirmationDialog from './ConfirmationDialog';
+import ManageUserTable from './ManageUserTable';
+
+const FederalAdmin = ({
+
+}) => {
+  const [activeTab, setActiveTab] = useState('pending');
+  const [isFetching, setIsFetching] = useState(true);
+  const [isDenied, setIsDenied] = useState(true);
+
+  const [selectedAffiliation, setSelectedAffiliation] = useState();
+
+  const [manageModalDisplay, setManageModalDisplay] = useState(false);
+  const [confirmationModalDisplay, setConfirmationModalDisplay] = useState(
+    false
+  );
+
+  useEffect(() => {
+
+  }, []);
+
+  useEffect(() => {
+
+  }, []);
+
+  const currentTab = id => {
+    setActiveTab(id);
+  };
+
+  const showManageModal = event => {
+
+  };
+
+  const hideManageModal = () => {
+    setManageModalDisplay(false);
+  };
+
+  const handleAffiliationUpdate = roleId => {
+
+  };
+
+  const showConfirmationModal = event => {
+
+  };
+
+  const hideConfirmationModal = () => {
+    setConfirmationModalDisplay(false);
+  };
+
+  const handleDenyOrRevoke = () => {
+
+  };
+
   return (
-    <Fragment>
-      <div className="site-body ds-l-container">
-        <div className="ds-l-row">
-          <div className="ds-l-col--8 ds-u-margin-x--auto ds-u-padding-y--6 admin-top-padding">
-            <h1 className="ds-h1">Administrator actions</h1>
-            <ul className="ds-c-list--bare ds-l-row">
-              <li className="ds-l-col--12 ds-l-md-col--6 ds-u-padding-left--0">
-                <h2 className="ds-h2">
-                  <div className="user-icon">
-                    <Icon icon={faUserCog} />
-                  </div>
-                  <Link
-                    to={{ pathname: '/create-account' }}
-                    className="ds-u-display--block"
+    <main
+      id="start-main-content"
+      className="ds-l-container ds-u-margin-bottom--5"
+    >
+      <h1>eAPD Federal Administrator Portal</h1>
+      <h2>State Administrator Requests</h2>
+      <Tabs onChange={currentTab}>
+        <TabPanel id="pending" tab="Requests">
+          <Table borderless>
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell>Email</TableCell>
+              <TableCell>Phone Number</TableCell>
+              <TableCell>State</TableCell>
+              <TableCell>Actions</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell>Tif Forkner</TableCell>
+              <TableCell>tif@fearless.tech</TableCell>
+              <TableCell>
+                <a href="# ">4108675309</a>
+              </TableCell>
+              <TableCell>Maryland</TableCell>
+              <TableCell>
+                <Button
+                  onClick={showManageModal}
+                  size="small"
+                  className="ds-u-margin-right--2"
+                  key="action1"
+                >
+                  Approve
+                </Button>
+                <Button
+                  onClick={showConfirmationModal}
+                  size="small"
+                  variation="danger"
+                  data-deny-or-revoke="deny"
+                  key="action2"
+                >
+                  Deny
+                </Button>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Ty Bolt</TableCell>
+              <TableCell>tbolt@fearless.tech</TableCell>
+              <TableCell>
+                <a href="# ">4108675309</a>
+              </TableCell>
+              <TableCell>New York</TableCell>
+              <TableCell>
+                <Button
+                    onClick={showManageModal}
+                    size="small"
+                    className="ds-u-margin-right--2"
+                    key="action1"
                   >
-                    Create new user account
-                  </Link>
-                </h2>
-                <p>Make new accounts for federal analysts and state filers</p>
-              </li>
-              <li className="ds-l-col--12 ds-l-md-col--6 ds-u-padding-right--0">
-                <h2 className="ds-h2">
-                  <div className="user-icon">
-                    <Icon icon={faUserPlus} />
-                  </div>
-                  <Link
-                    to={{ pathname: '/edit-account' }}
-                    className="ds-u-display--block"
+                    Approve
+                  </Button>
+                  <Button
+                    onClick={showConfirmationModal}
+                    size="small"
+                    variation="danger"
+                    data-deny-or-revoke="deny"
+                    key="action2"
                   >
-                    Manage user accounts
-                  </Link>
-                </h2>
-                <p>
-                  Edit account details and authorization levels for existing
-                  user accounts
-                </p>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </Fragment>
+                    Deny
+                  </Button>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+        </TabPanel>
+        <TabPanel id="active" tab="Active">
+
+        </TabPanel>
+        <TabPanel id="inactive" tab="Inactive">
+
+        </TabPanel>
+      </Tabs>
+
+      {confirmationModalDisplay && (
+        <ConfirmationDialog
+          hideConfirmationModal={hideConfirmationModal}
+          showConfirmationModal={showConfirmationModal}
+        />
+      )}
+
+      {manageModalDisplay && (
+        <ManageRoleDialog
+          hideManageModal={hideManageModal}
+          showManageModal={showManageModal}
+        />
+      )}
+    </main>
   );
 };
 
-export default FederalAdmin;
+FederalAdmin.propTypes = {
+
+};
+
+FederalAdmin.defaultProps = {
+
+};
+
+const mapDispatchToProps = {
+
+};
+
+const mapStateToProps = state => ({
+
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(FederalAdmin);
+
+export { FederalAdmin as plain, mapStateToProps, mapDispatchToProps };
